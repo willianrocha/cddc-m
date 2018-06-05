85b70441ba3e646a25897f2c49fef5b784c49799
Willian Vale da Rocha <willian.rocha@eldorado.org.br>
2018-06-05 10:12:27 -0400

    adicionado regra de filtros para retweet e uma lista de exceções

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import os
 import twitter
-from datetime import datetime 
+from datetime import datetime
+import json
+from collections import Counter
 
 c_key = os.environ['CONSUMER_KEY']
 c_secret = os.environ['CONSUMER_SECRET']
@@ -21,3 +23,24 @@ tweets_related =  api.GetSearch(term=HASHTAG_FIND, since=DATE_TODAY)
 ids_follow_back = set([t.user.id for t in tweets_related])
 for user_id in ids_follow_back:
     log = api.CreateFriendship(user_id=user_id)
+
+## Retweet filter
+# Retweets can be distinguished from typical Tweets by the existence of a 
+# retweeted_status attribute. 
+def filter_retweet(list_tweets):
+    return [t for t in list_tweets if not 'retweeted_status' in t.keys()]
+# Maybe useless
+def filter_exception_list(list_tweets):
+    exception_list = [
+        1003579193907654657, # cemdiasdecodigo
+        1003848514823213056, # _30days30sites
+    ]
+    return [t for t in list_tweets if not t['user']['id'] in exception_list]
+
+## Count filtering
+# Convert to dictionary, 'cause reasons
+tweets_dict = [json.loads(str(t)) for t in tweets_related]
+
+valid_tweets = filter_retweet(tweets_dict)
+valid_tweets = filter_exception_list(valid_tweets)
+print(valid_tweets)
